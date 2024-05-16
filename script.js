8bd5b9b92c309e3689ec085d7ebc3d82d133b5d8
eivor9 <njeremiah@pursuit.org>
2024-05-16 13:41:36 -0400

    keep track of user input and test against given sequence

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const button = document.querySelector("button");
+let levelHeader = document.querySelector("h1");
 const container = document.querySelector(".container");
 const tiles = document.querySelectorAll(".tile");
 const circularTiles = [tiles[0], tiles[1], tiles[3], tiles[5], tiles[4], tiles[2]];
@@ -10,6 +12,11 @@ const colors = [
     {dark: "rgba(255, 0, 255, 0.5)", light: "rgba(255, 0, 255, 1)"}
 ]
 
+for (let i = 0; i < circularTiles.length; i++){
+    circularTiles[i].addEventListener("mousedown", (e) => e.target.style.background = colors[i].light);
+    circularTiles[i].addEventListener("mouseup", (e) => e.target.style.background = colors[i].dark);
+}
+
 class Sequence {
     constructor (tile, index){
         this.tile = tile;
@@ -20,33 +27,57 @@ class Sequence {
 let level = 1;
 let simonSequence = [];
 let buttonSequence = [];
+button.addEventListener("click", game);
 
 for(let i = 0; i < circularTiles.length; i++){
     circularTiles[i].style.backgroundColor = colors[i].light;
 }
-for(let i = 1; i <= circularTiles.length; i++){
-    setTimeout(() => circularTiles[i-1].style.backgroundColor = colors[i-1].dark, i * 125);
+
+addClickListeners();
+
+function checkAnswer(){
+    removeClickListeners();
+    for(let i = 0; i < simonSequence.length; i++){
+        if(simonSequence[i].tile !== buttonSequence[i])
+            return false;
+    }
+    return true;
+}
+
+function addButton(e){
+    buttonSequence.push(e.target);
+    if (buttonSequence.length === simonSequence.length){
+        if(checkAnswer()){
+            continueGame();
+        }
+        else{
+            button.style.background = "linear-gradient(rgba(255, 0, 0, 1), rgba(255 ,0, 0, 0.5))";
+            button.style.color = "white";
+            button.innerText = "Game Over";
+            level = 1;
+            simonSequence = [];
+            buttonSequence = [];
+            button.addEventListener("click", game);
+        }
+    }
 }
 
 function game(){
-    console.log(simonSequence);
-    let counter = 0;
-    let gameRunning = setInterval(() => {
-        if (counter === level - 1){
-            let [currentTile, currentIndex] = randomTile();
-            flashTile(currentTile, currentIndex);
-            simonSequence.push(new Sequence(currentTile, currentIndex));
-            level++;
-            clearInterval(gameRunning);
-        } 
-        else{
-            let currentSequence = simonSequence[counter];
-            flashTile(currentSequence.tile, currentSequence.index);
-            counter++;
+    levelHeader.innerText = "Lvl " + level;
+    button.style.background = "rgba(255, 255, 255, 0.5)";
+    button.style.color = "black";
+    button.innerHTML = "..."
+    if(level === 1){
+        for(let i = 1; i <= circularTiles.length; i++){
+            setTimeout(() => circularTiles[i-1].style.backgroundColor = colors[i-1].dark, i * 125);
         }
-    }, 500);
+        setTimeout(mainLoop, 1000);
+    }
+    else{
+        mainLoop();
+    }
+    addClickListeners();
 }
-
 
 function flashTile(tile, index){
     tile.style.backgroundColor = colors[index].light;
@@ -56,4 +87,49 @@ function flashTile(tile, index){
 function randomTile(){
     let randomIndex = Math.floor(Math.random() * circularTiles.length);
     return [circularTiles[randomIndex], randomIndex];
+}
+
+function mainLoop(){
+    let counter = 0;
+    let gameRunning = setInterval(() => {
+        if (counter === level - 1){
+            let [currentTile, currentIndex] = randomTile();
+            flashTile(currentTile, currentIndex);
+            simonSequence.push(new Sequence(currentTile, currentIndex));
+            setTimeout(disableButton, 1000);
+            clearInterval(gameRunning);
+        } 
+        else{
+            let currentSequence = simonSequence[counter];
+            flashTile(currentSequence.tile, currentSequence.index);
+            counter++;
+        }
+    }, 750);
+}
+
+function disableButton(){
+    button.style.fontSize = "5rem";
+    button.innerText = "Your turn...";
+    button.removeEventListener("click", game);
+    
+}
+
+function continueGame(){
+    button.style.background = "linear-gradient(rgba(0, 255, 0, 1), rgba(0, 255, 0, 0.5))";
+    button.style.color = "white";
+    button.innerHTML = "Nice!";
+    button.addEventListener("click", game);
+    buttonSequence = [];
+    level++;
+}
+
+function addClickListeners(){
+    for(let i = 0; i < circularTiles.length; i++){
+        circularTiles[i].addEventListener("click", addButton);
+    }
+}
+function removeClickListeners(){
+    for(let i = 0; i < circularTiles.length; i++){
+        circularTiles[i].removeEventListener("click", addButton);
+    }
 }
