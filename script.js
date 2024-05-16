916dd245787cae76286266d635bb74da790e2be3
eivor9 <njeremiah@pursuit.org>
2024-05-15 22:09:30 -0400

    make buttons circular and troubleshoot main game loop

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
 const container = document.querySelector(".container");
-const holes = document.querySelectorAll(".hole");
-for(let i = 0; i < holes.length; i++){
-    holes[i].addEventListener("click", (e) => {
-        e.preventDefault();
-        testing.push(e.target);
-        console.log(testing);
-    })
+const tiles = document.querySelectorAll(".tile");
+const circularTiles = [tiles[0], tiles[1], tiles[3], tiles[5], tiles[4], tiles[2]];
+const colors = [
+    {dark: "rgba(255, 0, 0, 0.5)", light: "rgba(255, 0, 0, 1)"},
+    {dark: "rgba(255, 128, 0, 0.5)", light: "rgba(255, 128, 0, 1)"},
+    {dark: "rgba(255, 255, 0, 0.5)", light: "rgba(255, 255, 0, 1)"},
+    {dark: "rgba(0, 255, 0, 0.5)", light: "rgba(0, 255, 0, 1)"},
+    {dark: "rgba(0, 0, 255, 0.5)", light: "rgba(0, 0, 255, 1)"},
+    {dark: "rgba(255, 0, 255, 0.5)", light: "rgba(255, 0, 255, 1)"}
+]
+
+class Sequence {
+    constructor (tile, index){
+        this.tile = tile;
+        this.index = index;
+    }
 }
 
 let level = 1;
 let simonSequence = [];
-let testing = [];
+let buttonSequence = [];
 
-class Sequence {
-    constructor (tile, color){
-        this.tile = tile;
-        this.color = color;
-    }
+for(let i = 0; i < circularTiles.length; i++){
+    circularTiles[i].style.backgroundColor = colors[i].light;
+}
+for(let i = 1; i <= circularTiles.length; i++){
+    setTimeout(() => circularTiles[i-1].style.backgroundColor = colors[i-1].dark, i * 125);
 }
 
-function gameLoop(level){
-    for(let i = 0; i < level; i++){
-
-        let newButton = randomTile();
-        let newColor = psuedoRandomColor();
-
-        newButton.style.backgroundColor = newColor;
-        setTimeout(() => newButton.style.backgroundColor = "", 1000);
-
-        let newSequence = new Sequence(newButton, newColor);
-        simonSequence.push(newSequence);
+function game(){
+    for (let i = 1; i <= level; i++){
+        const [currentTile, currentIndex] = randomTile();
+        currentTile.style.backgroundColor = colors[currentIndex].light;
+        setTimeout(() => currentTile.style.backgroundColor = colors[currentIndex].dark, i*500);
+        simonSequence.push( new Sequence(currentTile, currentIndex) );
     }
+    level++;
 }
+
+
+
 
 function randomTile(){
-    let randomIndex = Math.floor(Math.random() * 10);
-    return holes[randomIndex];
+    let randomIndex = Math.floor(Math.random() * circularTiles.length);
+    return [circularTiles[randomIndex], randomIndex];
 }
-
-function psuedoRandomColor(){
-    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-    let randomIndex = Math.floor(Math.random() * 10);
-    return colors[randomIndex];
-}
-
