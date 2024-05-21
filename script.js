eb1b5bd140642989f6f9fc00bbdc73f191bbd364
eivor9 <njeremiah@pursuit.org>
2024-05-21 12:53:03 -0400

    add sounds and update button styling to be more 3d

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ const container = document.querySelector(".container");
 const tiles = document.querySelectorAll(".tile");
 const circularTiles = [tiles[0], tiles[1], tiles[3], tiles[5], tiles[4], tiles[2]];
 const colors = [
-    {dark: "rgba(255, 0, 0, 0.5)", light: "rgba(255, 0, 0, 1)"},
-    {dark: "rgba(255, 128, 0, 0.5)", light: "rgba(255, 128, 0, 1)"},
-    {dark: "rgba(255, 255, 0, 0.5)", light: "rgba(255, 255, 0, 1)"},
-    {dark: "rgba(0, 255, 0, 0.5)", light: "rgba(0, 255, 0, 1)"},
-    {dark: "rgba(0, 0, 255, 0.5)", light: "rgba(0, 0, 255, 1)"},
-    {dark: "rgba(255, 0, 255, 0.5)", light: "rgba(255, 0, 255, 1)"}
+    {dark: "rgba(255, 0, 0, 0.5)", light: "linear-gradient(rgba(255, 0, 0, 1), rgba(255, 0, 0, 0.5))"},
+    {dark: "rgba(255, 128, 0, 0.5)", light: "linear-gradient(rgba(255, 128, 0, 1), rgba(255, 128, 0, 0.5))"},
+    {dark: "rgba(255, 255, 0, 0.5)", light: "linear-gradient(rgba(255, 255, 0, 1), rgba(255, 255, 0, 0.5))"},
+    {dark: "rgba(0, 255, 0, 0.5)", light: "linear-gradient(rgba(0, 255, 0, 1), rgba(0, 255, 0, 0.5))"},
+    {dark: "rgba(0, 0, 255, 0.5)", light: "linear-gradient(rgba(0, 0, 255, 1), rgba(0, 0, 255, 0.5))"},
+    {dark: "rgba(255, 0, 255, 0.5)", light: "linear-gradient(rgba(255, 0, 255, 1), rgba(255, 0, 255, 0.5))"}
 ]
 
-// for (let i = 0; i < circularTiles.length; i++){
-//     circularTiles[i].addEventListener("mousedown", (e) => e.target.style.background = colors[i].light);
-//     circularTiles[i].addEventListener("touchstart", (e) => e.target.style.background = colors[i].light);
-//     circularTiles[i].addEventListener("mouseup", (e) => e.target.style.background = colors[i].dark);
-//     circularTiles[i].addEventListener("touchend", (e) => e.target.style.background = colors[i].dark);
-//     circularTiles[i].addEventListener("dblclick", (e) => e.preventDefault());
-// }
+let sounds = {
+    hit: new Audio("sounds/beep-29.wav"),
+    continue: new Audio("./sounds/button-37a.wav"),
+    gameover: new Audio("./sounds/beep-03.wav")
+}
+
+function playSound(soundName){                       
+    sounds[soundName].currentTime = 0;  
+    sounds[soundName].play();
+}
 
 class Sequence {
     constructor (tile, index){
@@ -33,7 +36,7 @@ let buttonSequence = [];
 button.addEventListener("click", game);
 
 for(let i = 0; i < circularTiles.length; i++){
-    circularTiles[i].style.backgroundColor = colors[i].light;
+    circularTiles[i].style.background = colors[i].light;
 }
 
 function checkAnswer(){
@@ -46,18 +49,20 @@ function checkAnswer(){
 }
 
 function addButton(e){
-    let temp = e.target.style.backgroundColor;
+    let temp = e.target.style.background;
     e.target.style.background = "white";
-    setTimeout(() => e.target.style.background = temp, 64);
+    setTimeout(() => e.target.style.background = temp, 125);
+    playSound("hit");
     buttonSequence.push(e.target);
     if (buttonSequence.length === simonSequence.length){
         if(checkAnswer()){
             continueGame();
         }
         else{
-            button.style.background = "linear-gradient(rgba(255, 0, 0, 1), rgba(255 ,0, 0, 0.5))";
+            button.style.background = "linear-gradient(rgba(255, 0, 0, 1), rgba(255 ,0 , 0, 0.5))";
             button.style.color = "white";
             button.innerText = "Game Over";
+            playSound("gameover");
             level = 1;
             simonSequence = [];
             buttonSequence = [];
@@ -67,24 +72,29 @@ function addButton(e){
 }
 
 function game(){
+    playSound("continue");
     levelHeader.innerText = "Lvl " + level;
     button.style.background = "rgba(255, 255, 255, 0.5)";
     button.style.color = "black";
     button.innerHTML = "..."
     if(level === 1){
-        for(let i = 1; i <= circularTiles.length; i++){
-            setTimeout(() => circularTiles[i-1].style.backgroundColor = colors[i-1].dark, i * 125);
+        for(let i = 2; i <= circularTiles.length; i++){
+            setTimeout(() => circularTiles[i-1].style.background = colors[i-1].dark, i * 125);
         }
+        setTimeout(() => circularTiles[0].style.background = colors[0].dark, 875);
         setTimeout(mainLoop, 1000);
     }
     else{
+        for(let i = 0; i < circularTiles.length; i++){
+            circularTiles[i].style.background = colors[i].dark;
+        }
         mainLoop();
     }
 }
 
 function flashTile(tile, index){
-    tile.style.backgroundColor = colors[index].light;
-    setTimeout(() => tile.style.backgroundColor = colors[index].dark, 500);
+    tile.style.background = colors[index].light;
+    setTimeout(() => tile.style.background = colors[index].dark, 500);
 }
 
 function randomTile(){
@@ -112,7 +122,7 @@ function mainLoop(){
 }
 
 function disableButton(){
-    button.style.fontSize = "8vh";
+    button.style.fontSize = "7vh";
     button.innerText = "Your turn...";
     button.removeEventListener("click", game);
     
@@ -130,6 +140,7 @@ function continueGame(){
 function addClickListeners(){
     for(let i = 0; i < circularTiles.length; i++){
         circularTiles[i].addEventListener("click", addButton);
+        circularTiles[i].style.background = colors[i].light;
     }
 }
 function removeClickListeners(){
